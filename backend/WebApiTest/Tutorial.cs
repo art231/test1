8d132bfe96cb608a57033b31d32a0a794a0fff6b
Dzakhov Artem <Dzakhov.Artem@infotecs-nt>
2022-08-03 10:21:39 +0300

    initial app

---
+++ backend/WebApiTest/Tutorial.cs
@@ -0,0 +1,14 @@
+namespace WebApiTest
+{
+    public class Tutorial
+    {
+        public string? Title { get; set; }
+
+        public DateTime LastStatistics { get; set; }
+
+        public string? VersionClient { get; set; }
+
+        public string? Type { get; set; }
+    }
+    
+}
---
+++ frontend/src/app/components/tutorials-list/tutorials-list.component.ts
@@ -0,0 +1,33 @@
+import { Component, OnInit } from '@angular/core';
+import { Tutorial } from 'src/app/models/tutorial.model';
+import { TutorialService } from 'src/app/services/tutorial.service';
+
+@Component({
+  selector: 'app-tutorials-list',
+  templateUrl: './tutorials-list.component.html',
+  styleUrls: ['./tutorials-list.component.css']
+})
+export class TutorialsListComponent implements OnInit {
+
+  tutorials?: Tutorial[];
+  currentTutorial: Tutorial = {};
+  currentIndex = -1;
+  title = '';
+
+  constructor(private tutorialService: TutorialService) { }
+
+  ngOnInit(): void {
+    this.retrieveTutorials();
+  }
+
+  retrieveTutorials(): void {
+    this.tutorialService.getAll()
+      .subscribe({
+        next: (data) => {
+          this.tutorials = data;
+          console.log(data);
+        },
+        error: (e) => console.error(e)
+      });
+  }
+}
